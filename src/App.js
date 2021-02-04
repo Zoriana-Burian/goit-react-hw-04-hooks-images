58c5aa1bf6613de4f3bbd20111f8f7bb32447470
Zoriana-Burian <64468262+Zoriana-Burian@users.noreply.github.com>
2021-02-04 20:55:59 +0200

    hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //import logo from './logo.svg';
 //import './App.css';
-import React, {Component, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 //import axios from 'axios';
 import newApi from './services/api-services';
 import Searchbar from './components/Searchbar/Searchbar';
@@ -10,166 +10,173 @@ import Modal from './components/Modal/Modal';
 import LoaderSpiner from './components/Loader/Loader';
 import Section from './components/Section/Section';
 
-// export default function App() {
-//   const [hits, setHits] = useState([]);
-//   const [currentPage, setCurrentPage] = useState(1);
-//   const [searchQuery, setSearchQuery] = useState('');
-//   const [largeImageURL, setLargeImageURL] = useState('');
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [showModal, setShowModal] = useState(false);
-//   const [error, setError] = useState(null);
+export default function App() {
+  const [hits, setHits] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [error, setError] = useState(null);
 
-// const onChangeQuery = query => {
-//       setSearchQuery(query);
-//       setCurrentPage(1);
-//       setHits([]);
-//       setError(null);
-//       setIsLoading(true);
-// }
+ useEffect(() => {
+    if (!searchQuery) {
+      return;
+    }
+
+    const fetchHits = () => {
+      setIsLoading(true);
+
+      newApi.fetchHitsImages({ searchQuery, currentPage })
+        .then(hits => {
+          setHits(prevHits => [...prevHits, ...hits]);
+        })
+        .catch(error => setError(error))
+        .finally(() => setIsLoading(false));
+    };
+
+    fetchHits();
+  }, [currentPage, searchQuery]);
+
+  const onChangeQuery = query => {
+      setSearchQuery(query);
+      setCurrentPage(1);
+      setHits([]);
+      setError(null);
+}
+    const onToggleModal = () => {
+      setShowModal(PrevShowModal => !showModal);
+    }
   
-//   useEffect(() => {
-//     if (!searchQuery) {
-//       return;
-//     }
-
- 
-//   })
-
-    
-
-//     const onToggleModal = () => {
-//       setShowModal((showModal) => ({
-//         showModal: !showModal,
-//       }));
-//     }
-  
-//     const clickOpenModal = e => {
-//       setLargeImageURL(e.target.dataset.source);
-//       onToggleModal();
-//     };
-
-//     const scrollImages = () => {
-//       window.scrollBy({
-//         top: document.documentElement.clientHeight - 100,
-//         behavior: 'smooth',
-//       });
-//     }
-
-//     const onLoadeMore = () => {
-//       setCurrentPage(prevCurrentPage => currentPage + 1)
-//     };
+    const clickOpenModal = e => {
+      setLargeImageURL(e.target.dataset.source);
+      onToggleModal();
+    };
 
 
-//     return (
-//       <div>
-//         {error && <h1> Перезавантажте, будь ласка, ще раз сторінку </h1>}
-//         <Section>
+
+    const scrollImages = () => {
+      window.scrollBy({
+        top: document.documentElement.clientHeight - 100,
+        behavior: 'smooth',
+      });
+    }
+
+    const onLoadeMore = () => {
+      setCurrentPage(prevCurrentPage => currentPage + 1)
+    };
+
+
+    return (
+      <div>
+        {error && <h1> Перезавантажте, будь ласка, ще раз сторінку </h1>}
+        <Section>
         
-//           {showModal && <Modal onClickModal={onToggleModal} largeImageURL={largeImageURL} />}
+          {showModal && <Modal onClickModal={onToggleModal} largeImageURL={largeImageURL} />}
 
-//           <Searchbar onSubmit={onChangeQuery} />
-//           <ImageGallery hits={hits} clickOpenModal={clickOpenModal} />
+          <Searchbar onSubmit={onChangeQuery} />
+          <ImageGallery hits={hits} clickOpenModal={clickOpenModal} />
 
-//           {isLoading && <LoaderSpiner />}
+          {isLoading && <LoaderSpiner />}
       
-//           {hits.length > 0 && !isLoading &&
-//             <Button onButton={onLoadeMore} onScroll={scrollImages()} />
-//           }
-//         </Section>
+          {hits.length > 0 && !isLoading &&
+            <Button onButton={onLoadeMore} onScroll={scrollImages()} />
+          }
+        </Section>
       
       
-//       </div>
-//     )
+      </div>
+    )
+  }
+  
+
+
+// class App extends Component {
+//   state = {
+//     hits: [],
+//     currentPage: 1,
+//     searchQuery: '',
+//     largeImageURL: '',
+//     isLoading: false,
+//     showModal: false,
+//     error: null,
+//   };
+
+//   onToggleModal = () => {
+//     this.setState(({ showModal }) => ({
+//       showModal: !showModal,
+//     })); 
+//   }
+
+//   clickOpenModal = e => {
+//     this.setState({
+//       largeImageURL: e.target.dataset.source}
+//   )
+//     this.onToggleModal();
+//   }
+
+//   scrollImages = () => {
+//      window.scrollBy({
+//   top: document.documentElement.clientHeight - 100,
+//   behavior: 'smooth',
+// });
+//   }
+ 
+ 
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevState.searchQuery !== this.state.searchQuery) {
+//       this.fetchHits();
+//     }
+//   }
+
+//   onChangeQuery = query => {
+//     this.setState({ searchQuery: query, currentPage: 1, hits: [], error: null, });
+    
+//   }
+  
+//   fetchHits = () => {
+    
+//     const { currentPage, searchQuery } = this.state;
+    
+//     this.setState({ isLoading: true });
+  
+//     const options = { currentPage, searchQuery };
+    
+//     newApi.fetchHitsImages(options)
+//       .then(hits => {
+//         this.setState(prevState => ({
+//           hits: [...prevState.hits, ...hits],
+//           currentPage: prevState.currentPage + 1,
+//         }))
+//       })
+//       .catch(error => this.setState({error}))
+//       .finally(() => this.setState({ isLoading: false }));
+    
+//     //this.scrolImages();
 //   }
   
 
+//   render() {
+//     const { hits, isLoading, showModal, largeImageURL, error } = this.state;
 
-class App extends Component {
-  state = {
-    hits: [],
-    currentPage: 1,
-    searchQuery: '',
-    largeImageURL: '',
-    isLoading: false,
-    showModal: false,
-    error: null,
-  };
-
-  onToggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    })); 
-  }
-
-  clickOpenModal = e => {
-    this.setState({
-      largeImageURL: e.target.dataset.source}
-  )
-    this.onToggleModal();
-  }
-
-  scrollImages = () => {
-     window.scrollBy({
-  top: document.documentElement.clientHeight - 100,
-  behavior: 'smooth',
-});
-  }
- 
- 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.searchQuery !== this.state.searchQuery) {
-      this.fetchHits();
-    }
-  }
-
-  onChangeQuery = query => {
-    this.setState({ searchQuery: query, currentPage: 1, hits: [], error: null, });
-    
-  }
-  
-  fetchHits = () => {
-    
-    const { currentPage, searchQuery } = this.state;
-    
-    this.setState({ isLoading: true });
-  
-    const options = { currentPage, searchQuery };
-    
-    newApi.fetchHitsImages(options)
-      .then(hits => {
-        this.setState(prevState => ({
-          hits: [...prevState.hits, ...hits],
-          currentPage: prevState.currentPage + 1,
-        }))
-      })
-      .catch(error => this.setState({error}))
-      .finally(() => this.setState({ isLoading: false }));
-    
-    //this.scrolImages();
-  }
-  
-
-  render() {
-    const { hits, isLoading, showModal, largeImageURL, error } = this.state;
-
-    return <div>
-      {error && <h1> Перезавантажте, будь ласка, ще раз сторінку </h1>}
-      <Section>
+//     return <div>
+//       {error && <h1> Перезавантажте, будь ласка, ще раз сторінку </h1>}
+//       <Section>
         
-        {showModal && <Modal onClickModal={this.onToggleModal} largeImageURL={largeImageURL} />}
+//         {showModal && <Modal onClickModal={this.onToggleModal} largeImageURL={largeImageURL} />}
 
-      <Searchbar onSubmit={this.onChangeQuery}/>
-      <ImageGallery hits={hits} clickOpenModal={this.clickOpenModal} />
+//       <Searchbar onSubmit={this.onChangeQuery}/>
+//       <ImageGallery hits={hits} clickOpenModal={this.clickOpenModal} />
 
-      {isLoading && <LoaderSpiner/>}
+//       {isLoading && <LoaderSpiner/>}
       
-      {hits.length > 0 && !isLoading &&
-          <Button onButton={this.fetchHits} onScroll={ this.scrollImages()}/>
-    }
-      </Section>
+//       {hits.length > 0 && !isLoading &&
+//           <Button onButton={this.fetchHits} onScroll={ this.scrollImages()}/>
+//     }
+//       </Section>
       
       
-    </div>;
-  }
-}
-export default App;
+//     </div>;
+//   }
+// }
+// export default App;
